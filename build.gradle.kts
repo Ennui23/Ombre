d62430660308aa461881f5bb0fbd88c038e4b69d
Jed A. Divinagracia <divinagraciajed.dev@gmail.com>
2025-07-07 06:56:13 +0800

    Update: Fixed JVM target and improved README for public use

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,4 +7,5 @@ buildscript {
 plugins {
     id("com.android.application") version "8.2.1" apply false
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
+    id("org.jetbrains.kotlin.jvm") version "1.8.0" apply false
 }
